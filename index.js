765a8a4dcf42e80cf027c83f9c17879e12314d38
Junior Medina <67336861+jr1412594@users.noreply.github.com>
2021-09-30 16:47:55 +0000

    ternary check

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const age = 19;
-// const age = 16;
+//TINKER WITH IF ELSE STATEMENTS
+
+// const age = 19;
+const age = 16;
 const isOldEnough = age >= 18;
+const yearsLeft = 18 - age
 
 // if (isOldEnough === true){
 //   console.log(`Sarah can drive ☠️`);
 // } else {
-//   console.log(`get the f outta here!`)
+//   console.log(`You still have ${yearsLeft} years till you can drive`)
 // }
 
 isOldEnough 
   ? console.log(`Sarah can drive ☠️`)
-  :  console.log(`get the f outta here!`)
+  :  console.log(`You still have ${yearsLeft} till you can drive!`)
