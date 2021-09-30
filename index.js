bedc9cbe583dd9c52783a4057d31ff6c4176dd6d
jrMedina <>
2021-09-30 16:38:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const age = 19;
+// const age = 16;
+const isOldEnough = age >= 18;
+
+// if (isOldEnough === true){
+//   console.log(`Sarah can drive ☠️`);
+// } else {
+//   console.log(`get the f outta here!`)
+// }
+
+isOldEnough 
+  ? console.log(`Sarah can drive ☠️`)
+  :  console.log(`get the f outta here!`)
